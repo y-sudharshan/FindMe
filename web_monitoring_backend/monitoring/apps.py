4e06d80ebe2af6752fbaf1374b45578d7857f726
y-sudharshan <sudharshanr2017@gmail.com>
2026-01-09 19:09:14 +0530

    apps

---
+++ web_monitoring_backend/monitoring/apps.py
@@ -0,0 +1,8 @@
+"""
+Monitoring app configuration
+"""
+from django.apps import AppConfig
+
+class MonitoringConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'monitoring'
